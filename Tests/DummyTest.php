1c807558eb4f6eb4683e7a1a81906929519f2d00
Matthias Schobner <matthias.schobner@me.com>
2019-10-10 14:52:51 +0200

    add todo's

---
+++ Tests/DummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+// TODO: DependencyInjection tests
+//  -> email_log_entity with config.yml
+
+// TODO: EventListener test
+//  -> Email send (not breaked)?
+//  -> Create log test (test all parameters from email!)
+//  -> Update log test
+//      -> New result status
+//      -> Errors saved? (Exception don't break sending email?!
+
+// TODO: Test multiple PHP & Symfony & Doctrine & SwiftMailer versions
